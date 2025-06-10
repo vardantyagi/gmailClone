e29e65a7481ea2fc44b6d28fd35715ac68b3256e
Vardan Tyagi <vardantyagii@gmail.com>
2025-06-10 15:10:39 +0530

    updated login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:3000/api/v1/user/login', input,
+      const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/v1/user/login`, input,
         { withCredentials: true }
       );
       if (res.data.success) {
